7c7e5eca8676840467b223aa4887afc9ff6574f2
MatyBotta <Bottarinimatias03@gmail.com>
2021-07-08 09:09:04 -0300

    Update main.c
    Correccion 1

--- EjePractica_uso_de_repositorios/main.c
+++ EjePractica_uso_de_repositorios/main.c
@@ -5,12 +5,13 @@ int main()
 {
     float n1,n2,n3,r;
     printf("ingrese la nota 1");
-    scanf("%d"&n1);
+    scanf("%f"&n1);
     printf("ingrese la nota 2");
     scanf("%d"&n2);
     printf("ingrese la nota 3");
-    scanf("%d"&n3);
+    scanf("%f"&n3);
     r=(n1+n2+n3)/3;
     printf("El promedio es: %f", r);
+
     return 0;
 }
