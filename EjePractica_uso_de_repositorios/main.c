e0ec27aea9e06802723dacd44d934c9d749f4752
dejameponerunnombre <alumno.paltamiranda@gmail.com>
2021-07-08 08:44:37 -0300

    Update main.c

--- EjePractica_uso_de_repositorios/main.c
+++ EjePractica_uso_de_repositorios/main.c
@@ -5,7 +5,10 @@ int main()
 {
     float n1,n2,n3,r;
     printf("ingrese la nota 1");
+    scanf("%d"&n1);
     printf("ingrese la nota 2");
+    scanf("%d"&n2);
     printf("ingrese la nota 3");
+    scanf("%d"&n3);
     return 0;
 }
